2c028d45e54b0fa74a9c92db051860ecc62ec8d2
luanvn2203 <luanvnse63360@gmail.com>
2021-06-11 08:01:49 +0700

    test post login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import React from "react";
 import Routes from "../src/routes/routes";
-import HomePage from "./pages/Home/HomePage";
-import LoginPage from "./pages/Login/LoginPage";
-import RegisterPage from "./pages/Register/RegisterPage";
-import {
-	BrowserRouter as Router,
-	Route,
-	Link,
-	Switch
-} from 'react-router-dom';
 
 function App() {
 	return (
--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
 import "antd/dist/antd.css";
-import { Form, Input, Button, Checkbox } from "antd";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Images from "../../constants/Images";
+import React, { useEffect, useState } from "react";
+import accountApi from "../../api/accountApi";
 import "../../assets/css/account.css";
+import LoginForm from "../../components/LoginForm";
+import Images from "../../constants/Images";
+import axios from "axios"
+const LoginPage = function () {
+	const [loginValue, setLoginValue] = useState({})
+	//////
 
-const LoginPage = () => {
-	const [Values, setValues] = useState({});
+	//////	
+	async function handleLoginFormSubmit(formValues) {
+		setLoginValue(formValues)
+	}
 
-	const onFinish = (values) => {
-		setValues(values);
+	useEffect(() => {
+		const fetchLogin = async () => {
+			try {
+				const response = await accountApi.Login(loginValue);
+				console.log(response)
 
-	};
+			} catch (error) {
+				console.log("failed to fetch login", error.message)
+			}
+		}
+		fetchLogin();
+	}, [loginValue])
 
-	console.log("Received values of form: ", Values);
 	return (
 		<div className="limiter">
 			<div className="container">
@@ -21,65 +34,7 @@ const LoginPage = () => {
 					<div className="pic js-tilt" data-tilt>
 						<img src={Images.LOGIN_SVG} alt="login" />
 					</div>
-
-					<Form
-						name="normal_login"
-						className="form-control"
-						initialValues={{ remember: true }}
-						onFinish={onFinish}
-					>
-						<span className="form-control-title">Login From</span>
-						<Form.Item
-							name="email"
-							className="form-group"
-							rules={[{ required: true, message: "Please input your Email!" }]}
-						>
-							<Input
-								className="form-input"
-								type="email"
-								placeholder="Email"
-								required
-							/>
-						</Form.Item>
-						<Form.Item
-							name="password"
-							className="form-group"
-							rules={[
-								{ required: true, message: "Please input your Password!" },
-							]}
-						>
-							<Input
-								className="form-input"
-								type="password"
-								placeholder="Password"
-								required
-							/>
-						</Form.Item>
-						<Form.Item className="form-group txt2">
-							<Form.Item
-								name="remember"
-								className="form-input form-check"
-								valuePropName="checked"
-								noStyle
-							>
-								<Checkbox>Remember me</Checkbox>
-							</Form.Item>
-
-							<Link className="login-form-forgot link-a" to="">
-								Forgot password
-							</Link>
-						</Form.Item>
-						<Form.Item className="form-group">
-							<Button htmlType="submit" className="form-control-btn">
-								Login
-							</Button>
-						</Form.Item>
-						<Form.Item className="form-group text-center p-t-50">
-							<Link className="link-a" to="/register">
-								Create your account
-							</Link>
-						</Form.Item>
-					</Form>
+					<LoginForm onSubmit={handleLoginFormSubmit} />
 				</div>
 			</div>
 		</div>
